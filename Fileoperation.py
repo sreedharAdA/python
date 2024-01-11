983672dbfc19384b8824a629019b8064d2fbbd02
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-01-11 11:40:53 +0530

    Fileoperations

---
+++ Fileoperation.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+filepath = "c://test"
+# os.mkdir(filepath+"/python")
+# print("the folder is created")
+# os.mkdir(filepath+"/python")
+print("the sub folder is created")
+os.chdir(filepath)
+sourcepath = "c://test"
+destinationpath = "c://test"
+# shutil.copy(sourcepath, destinationpath)
+print("file is copied completed")
+# os.rmdir(filepath)
+# print("filepath  is remove")
+
+
