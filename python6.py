7f6b2483185230cb3a244b5a4474155f321113e5
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-01-26 14:33:04 +0530

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,28 @@
+# created by sreedhar
+# 24/01/2024
+# list = [1, 2, 3, 4, 5, 7, 77, 89]
+# list1 = list
+# list1 = [97,86,54,32,22,11,]
+# print(list1)
+# print(list)
+
+# list = [1, 2, 3, 4, 5, 7, 77, 89]
+# list1 = list
+# print(list)
+# print(id(list1))
+# list1 = [7, 77, 777, 7777, 77777, 777777]
+# print(id(list))
+# print(id(list1))
+#
+# a = 10
+# b = a
+# print(a)
+# print(b)
+#
+# a = 10
+# b = a
+# a = 5
+#
+# print(a)
+# print(b)
+
