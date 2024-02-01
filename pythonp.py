90c646140359062b87f0078905206fa040f5b654
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-02-01 15:39:35 +0530

    Add files via upload

---
+++ pythonp.py
@@ -0,0 +1,113 @@
+# created by sreedhar
+# 1/02/2024
+
+a = 12
+b = 40
+print(a+b)
+
+a = 12
+b = 40
+print(a-b)
+
+a = 12
+b = 40
+print(a/b)
+
+a = 12
+b = 40
+print(a*b)
+
+a = 12
+b = 40
+print(a%b)
+
+a = "sreedhar"
+b = "123"
+print(a+b)
+print(id(a+b))
+a = "ravi"
+b = "deva"
+print(a+b)
+print(id(a+b))
+
+a = 12
+b = 22.09
+print(a+b)
+
+a = 44
+b = 55.0j
+print(a+b)
+
+# a = "sreedhar"
+# b = 22.03
+# print(a+b)
+#
+# a = "12"
+# b = 40
+# print(a+b)
+
+# a = "ee"
+# b = 12.0j
+# print(a+b)
+
+
+
+
+
+# import os
+# import shutil
+# Filepath = "c://sreedhar"
+# # print("the folder is created")
+# # # os.mkdir(Filepath+"/deva")
+# print("the sub folder is created")
+# sourcepath = "c://sreedhar/deva/copied.txt"
+# destinationpath = "//deva/copied.txt"
+#
+# # Copy the file
+# shutil.copy(sourcepath, destinationpath)
+# print("The file is copied successfully")
+#
+#
+#
+#
+# FilePath = "c://sa/copied.txt"
+# sreedhar = open(FilePath, "r")
+# MyData = sreedhar.read()
+# print("the filepath is read:", MyData)
+#
+# # readline
+#
+# sreedhar = open(FilePath, "a+")
+# sreedhar.seek(10)
+# print(sreedhar.tell())
+#
+# MyData = sreedhar.readline(20)
+# print("the filepath is readline:", MyData)
+#
+# # readlines
+#
+# sreedhar = open(FilePath, "r")
+# # sreedhar.seek(0)
+# sreedhar.tell()
+# MyData = sreedhar.readlines()
+# print("the filepath is readlines:", MyData)
+#
+# # write
+# FilePath = "c://sa1/copied.txt"
+# sreedhar = open(FilePath, "w+")
+# MyData = "sreedhar is good crickete"
+# sreedhar.write(MyData)
+# sreedhar.close()
+#
+#
+# # write lines
+# FilePath = "c://sa1/copied.txt"
+# sreedhar = open(FilePath, "w+")
+# MyData = "sreedhar is good cricketer"
+# sreedhar.write(MyData)
+#
+# strlist = ["sreedhar", "deva", "arun"]
+# sreedhar.writelines(str(strlist))
+# sreedhar.close()
+#
+#
