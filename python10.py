5ce7adf47c196b8da0dfc7c73eaab698c981e6d7
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-02-08 17:18:42 +0530

    08/07/2024

---
+++ python10.py
@@ -0,0 +1,81 @@
+# # created by sreedhar
+# # 08/02/2024
+#
+# class sreedhar():
+#     def funtion(self):
+#         print("sreedhar")
+# class deva(sreedhar):
+#     def funtion(self):
+#         print("deva")
+# obj = deva()
+# obj.funtion()
+#
+# # addition
+#
+# class add():
+#     def fun1(self,a,b):
+#         self.a = a
+#         self.b = b
+#         print(a+b)
+# class sub():
+#     def fun1(self,a,b):
+#         self.a = a
+#         self. b = b
+#         print(a-b)
+# class division():
+#     def fun1(self,a,b,c):
+#         self. a = a
+#         self. b =b
+#         print(a/b+c)
+# obj= division()
+# obj.fun1(1000,30,200)
+#
+#
+#
+# class int():
+#     def fun1(self,a,):
+#         self.a = a
+#         print(a)
+# class S():
+#     def fun1(self,a,b):
+#         self.a = a
+#         self. b = b
+#         print(a+b)
+# class SA():
+#     def fun1(self,a,b,c):
+#         self. a = a
+#         self. b = b
+#         self. c = c
+#         print(a+b+c)
+# obj= SA()
+# obj.fun1(23,27,50)
+
+
+#  datatype
+
+class s():
+    def fun2(self,a):
+        self. a=a
+        print(a)
+
+class sa():
+    def fun2(self,a,b):
+        self. a=a
+        self. b=b
+        print(int(a)+b)
+
+class sla():
+    def fun2(self,a,b,c):
+        self. a=a
+        self. b=b
+        self. c=c
+        print(int(a)+b-c)
+class sad():
+    def fun2(self,a,b,c,d):
+        self. a=a
+        self. b=b
+        self. c=c
+        self. d=d
+        print(int(a)-b+c/d)
+obj= sad()
+obj.fun2("10",23,20.0,23.0j)
