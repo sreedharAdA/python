70b8255aa4db4a5482effa1bd3a13dea42a59734
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-02-15 11:19:37 +0530

    14/02/2024

---
+++ numpyex.py
@@ -0,0 +1,61 @@
+# created by sreedhar
+# 14/02/2024
+# zero dimensional
+import numpy as np
+myarray = np.array([15])
+print("created array:",myarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+
+# one dimensional
+onedarray = np.array([1,2,3])
+print("created onedarray:",onedarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+
+# two dimensional
+twodarray = np.array([[1,2,3]])
+print("created array:",twodarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+
+
+# three dimensional
+threedarray = np.array([[[1,2,3]]])
+print("created array:",threedarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+
+
+# four dimensional
+fourdarray = np.array([[[[1,2,3]]]])
+print("created array:",fourdarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+
+
+# five dimensional
+fivedarray = np.array([[[[[1,2,3]]]]])
+print("created array:",fivedarray)
+print("the array size:",np.size)
+print("the array dimensional:",np.ndim)
+print("the shape of array:",np.shape)
+print("the datatype of array:",np.dtype)
+
+sreedhararray=np.ones(6,int)
+print(sreedhararray)
