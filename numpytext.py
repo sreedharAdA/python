cbe29599f0912df8093819e32247b53d230aa75d
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-03-04 17:16:46 +0530

    04/03/2024

---
+++ numpytext.py
@@ -0,0 +1,11 @@
+import numpy as np
+pydata=np.loadtxt("c:/sa.txt",skiprows=1,delimiter=',',dtype=int)
+print(pydata)
+# pdata=np.genfromtxt(skip_header=0)
+# print(pdata)
+
+# sadata=np.nan
+# print(sadata)
+
+# sa1data=np.savetxt("c:/sa.txt",delimiter="@",fmt="%",)
+# print(pydata)
