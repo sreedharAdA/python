4bac9f718e48b6f99bf536000d3b649854be86d9
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-03-04 17:17:31 +0530

    04/03/2024

---
+++ pn.py
@@ -0,0 +1,16 @@
+import pandas as pd
+print('pandas is created')
+sa1=([1,2,3,4,5,])
+sa=pd.Head(1)
+print(sa)
+sa=pd.tail(1)
+print(sa)
+
+print(sa)
+sa=pd.count(1)
+print(sa)
+
+
+print(sa)
+sa=pd.count(1)
+print(sa)
