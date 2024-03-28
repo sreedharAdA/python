f07cb0e3ebf43cf95443209fb206c594441dbc38
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-03-28 16:11:41 +0530

    28/03/2024

---
+++ pythonmain.py
@@ -0,0 +1,193 @@
+# created by sreedhar
+# 28/03/2024
+
+
+# comments
+
+# single line comments
+####################
+
+# multiple lines comments
+'''    '''
+
+
+# variable
+
+a=10
+print(a)
+
+# name+value
+
+# id
+a=10
+print(id(a))
+
+# type
+a=10
+print(type(a))
+
+
+# type convertion
+
+a='10'
+print(int(a))
+
+
+# Arithmetic operators
+"""
++
+-
+/
+%
+*
+//
+**
+"""
+a=20
+b=40
+print(a+b)
+print()
+
+a=20
+b=40
+print(a-b)
+print()
+
+a=20
+b=40
+print(a*b)
+print()
+
+a=20
+b=40
+print(a/b)
+print()
+
+a=20
+b=40
+print(a%b)
+print()
+
+a=20
+b=40
+print(a/b)
+print()
+
+a=20
+b=40
+print(a//b)
+print()
+
+a=20
+b=40
+print(a**b)
+
+
+# Relational opretaers
+"""
+<
+>
+<=
+>=
+!=
+==
+"""
+a=20
+b=40
+print(a<b)
+print()
+
+a=20
+b=40
+print(a<=b)
+print()
+
+a=20
+b=40
+print(a>=b)
+print()
+
+a=20
+b=40
+print(a==b)
+print()
+
+a=20
+b=40
+print(a!=b)
+print()
+
+# logical operators
+"""
+and 
+or
+not
+"""
+a=True
+b=False
+print(a and b)
+print()
+print(a or b)
+print()
+print(not b)
+print(not a)
+print()
+
+
+# Assignment operators
+
+"""
+=
++=
+*=
+/=
+//=
+**=
+%=
+
+"""
+a=23
+b=10
+a+=5
+print(a)
+print()
+a-=5
+print(a)
+print()
+a*=5
+print(a)
+print()
+
+a%=5
+print(a)
+print()
+
+a/=5
+print(a)
+print()
+
+a//=5
+print(a)
+print()
+
+a**=5
+print(a)
+print()
+
+
+
+# data type
+
+'''
+basic data type
+int
+float
+string
+complex
+ 
+advens data type
+list
+set
+tuple
+dictinary
+'''
