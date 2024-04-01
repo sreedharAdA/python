f6ead96663b71338d91f336ddce9931ac31ca60c
sreedhar <155968686+sreedharAdA@users.noreply.github.com>
2024-04-01 16:27:28 +0530

    01/04/2024

--- numpyex2.py
+++ numpyex2.py
@@ -1,18 +1,51 @@
 # created by sreedhar
-# 15/02/2024
+# 01/04/2024
+
 import numpy as np
-# sreedhar = np.array([[[0,1,2,3,4,],[0,1,2,3,4,]],[[11,22,33,44,55],[66,77,88,99,00]],[[21,22,23,24,25],[31,32,33,34,35]],[[41,42,43,44,45],[51,52,53,54,55]],[[61,62,63,64,65],[71,72,73,74,75]]])
-# print(sreedhar)
-# print("index value:",np.where(sreedhar)[2][1])
-#
-# # 2D
-# sreedhar2=np.array([[1,2,3],[4,5,6],[14,15,16]])
-# print(sreedhar2[2,1])
 
-# # 3D
-# sreedhar3=np.array([[[11,12,13],[14,15,16]],[[10,21,22],[23,24,25]]])
-# print(sreedhar3[1,1,2])
+arr = np.array([1, 2, 3, 4, 5])
 
-# 4D
-sreedhar4=np.array([[[[1,2,3],[4,5,6],[7,8,9]]]])
-print(sreedhar4[0,0,2,2])
+print(arr)
+
+print(type(arr))
+
+print()
+
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+print()
+
+arr = np.array(42)
+
+print(arr)
+print()
+
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+print()
+
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
+print()
+
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(arr)
+
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
